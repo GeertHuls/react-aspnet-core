c842b25b51e864da3cf1f56ac26321547fbaa38d
Geert Huls <geert.huls82@gmail.com>
2018-07-29 22:08:15 +0200

    Ready app for jsx
    - npm install babel-core babel-loader babel-preset-env babel-preset-react babel-preset-stage-2 react react-dom

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,22 @@ const config = {
         path: PUBLIC_DIR,
         filename: 'bundle.js'
     },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        'react', 'stage-2',
+                        ['env', { targets: { browsers: ['last 2 versions'] } }]
+                    ]
+                }
+            }
+        ]
+    }
 };
 
 module.exports = config;
