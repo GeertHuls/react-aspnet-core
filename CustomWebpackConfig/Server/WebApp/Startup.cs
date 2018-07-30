cd58ecbc2321b8fb209ca4cd9ac9885a95ce980a
Geert Huls <geert.huls82@gmail.com>
2018-07-30 21:55:54 +0200

    Ready aspnet core web app to serve static content sush as the react app

--- CustomWebpackConfig/Server/WebApp/Startup.cs
+++ CustomWebpackConfig/Server/WebApp/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApp
@@ -25,10 +20,13 @@ namespace WebApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            var options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("index.html");
+
+            app.UseDefaultFiles(options);
+
+            app.UseStaticFiles();
         }
     }
 }
