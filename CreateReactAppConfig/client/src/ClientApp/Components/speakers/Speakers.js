c798b28ee1e28e8eeb2cdddb0e7e374cbde619e1
Geert Huls <geert.huls82@gmail.com>
2018-07-31 22:41:05 +0200

    Load json data from url instead

--- CreateReactAppConfig/client/src/ClientApp/Components/speakers/Speakers.js
+++ CreateReactAppConfig/client/src/ClientApp/Components/speakers/Speakers.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import SpeakersHeader from './SpeakersHeader';
 import SpeakerList from './SpeakerList';
-//import axios from 'axios';
-
-
+import axios from 'axios';
 
 class Speakers extends Component {
 
@@ -11,54 +9,28 @@ class Speakers extends Component {
         super(props);
         this.state = {
             isLoading: true,
-            appData: [
-                {
-                    id:101,
-                    firstName: "Marc",
-                    lastName: "Andreesen",
-                    imageUrl: "assets/images/speakers/speaker1.png"
-                },
-                {
-                    id:102,
-                    firstName:"Llewellyn",
-                    lastName:"Falco",
-                    imageUrl:"assets/images/speakers/speaker2.png"
-                },
-                {
-                    id:103,
-                    firstName:"Stefania",
-                    lastName:"Kaczmarczyk",
-                    imageUrl:"assets/images/speakers/speaker3.png"
-                },
-                {
-                    id:104,
-                    firstName:"Diane",
-                    lastName:"Green",
-                    imageUrl:"assets/images/speakers/speaker0.png"
-                }
-            ]
+            appData: []
         };
     }
 
     componentDidMount() {
-
-        //const url = '/data/speakers.json';
+        const url = '/data/speakers.json';
         //const url = '/api/speaker';
 
         console.log('Speakers:cdm');
-        // axios.get(url)
-        //     .then((result)=> {
-        //         console.log('api/speaker then called');
-        //         this.setState({
-        //             appData: result.data,
-        //             isLoading: false
-        //         });
-        //     })
-        //     .catch(error => {
-        //         if (error.response) {
-        //             console.log(error.responderEnd);
-        //         }
-        //     });
+        axios.get(url)
+            .then((result)=> {
+                console.log('api/speaker then called');
+                this.setState({
+                    appData: result.data,
+                    isLoading: false
+                });
+            })
+            .catch(error => {
+                if (error.response) {
+                    console.log(error.responderEnd);
+                }
+            });
     }
 
 
